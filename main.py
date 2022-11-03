4c880b01dcb864f906aaa91f2f56e67a46eea6cc
Vasco Pinto <vasco20gouveia259@outlook.com>
2022-11-03 09:50:39 +0000

    Criando funções
    Criando um reconhecedor de voz, com suporte online.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #Arquivo principal
+
+#1.Importando o reconhecedor de Speech
+
+import speech_recognition as sr
+
+#2.Criando um reconhecedor de voz
+
+r = sr.Recognizer()
+
+#3. Abrindo o microfonhe para entrada de áudio
+
+with sr.Microphone() as source:
+#4. Definindo o Micriofone como entrada de áudio
+
+    audio = r.listen(source)
+
+    print(r.recognize_google(audio))
